85d9a418975561f5db2af7cab1f0757a7fa49a27
Brian Wilkinson <brian.joseph.wilkinson@gmail.com>
2024-04-30 22:08:20 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import './App.css';
-import Layout from './Layout'
-import { PrimeReactProvider } from 'primereact/api';
+import React from "react";
+import "./App.css";
+import Layout from "./Layout";
+import { PrimeReactProvider } from "primereact/api";
 
 function App() {
-
   return (
     <PrimeReactProvider>
-    <div className="App">
-   <h1>API</h1>
-   <Layout />
-    </div>
+      <div className="App">
+        <h1>API</h1>
+        <Layout />
+      </div>
     </PrimeReactProvider>
   );
 }
