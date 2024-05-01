e24e7f52c8e9993f834ed7bf912b489f979186f1
Brian Wilkinson <brian.joseph.wilkinson@gmail.com>
2024-05-01 16:55:27 -0700

    added REACT_APP_API

--- src/coinList.js
+++ src/coinList.js
@@ -14,7 +14,7 @@ const CoinList = () => {
   // Fetch data from the proxy server endpoint
   const fetchData = async () => {
     try {
-      const response1 = await axios.get("http://localhost:3001/api/coins");
+      const response1 = await axios.get(process.env.REACT_APP_API_URL);
 
       // Process response1 and response2 as needed
       const data1 = response1.data.data;
