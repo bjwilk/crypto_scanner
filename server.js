2eafd616ca919777b9835e1240d513b474bd2fc7
Brian Wilkinson <brian.joseph.wilkinson@gmail.com>
2024-04-30 20:04:45 -0700

    updated scripts

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 require('dotenv').config()
 
 const app = express();
-const PORT = 3001; // Choose any available port
+const PORT = process.env.PORT || 3001; // Choose any available port
 
 // Enable CORS middleware
 app.use(cors());
